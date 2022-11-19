2ca46dbeb364aa2e8450ab65a965e3176c8b013e
Eric Le Pen <eric.lepen@autoliv.com>
2022-11-20 00:59:25 +0100

    initialize fibonacci

---
+++ fibo.f90
@@ -0,0 +1,15 @@
+program main
+  implicit none
+  integer :: i
+  integer, parameter :: n=10
+  integer, dimension(n) :: numarray
+
+  numarray(1:2) = 1
+  do i=3,n
+    numarray(i) = numarray(i-2) + numarray(i-1)
+  end do
+
+  do i=1,n
+    print *, numarray(i)
+  end do
+end program main
